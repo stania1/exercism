11dc62f5278f22bf39dae301bd47dc9781cb56d7
Sofia Tania <stania@thoughtworks.com>
2018-04-15 20:21:40 -0400

    Leap without if

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -1,7 +1,5 @@
 object Leap {
   def leapYear(year: Int): Boolean = {
-    if (year % 100 == 0) return year % 400 == 0
-
-    year % 4 == 0
+    ( year % 400 == 0 ) || ( year % 100 != 0 && year % 4 == 0 )
   }
 }
