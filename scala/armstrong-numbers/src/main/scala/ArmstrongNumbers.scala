b3cdd6376b0a01b8e2720be2336cb3f85ff4ec63
Sofia Tania <stania@Sofias-MacBook-Pro.local>
2019-05-10 20:59:50 -0400

    Armstrong numbers

---
+++ scala/armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
@@ -0,0 +1,19 @@
+object ArmstrongNumbers {
+
+  def isArmstrongNumber(number: Int): Boolean = {
+
+    val digits = number.toString.map(_.asDigit)
+
+    val numDigits = digits.length
+
+    val criteria = digits
+      .map(digit => Math.pow(digit, numDigits))
+      .sum
+      .toInt
+
+    number == criteria
+    
+  }
+
+
+}
