550ba81b006a35951fbf5a7678c22d1991db6037
Sofia Tania <1446811+stania1@users.noreply.github.com>
2021-06-05 22:42:19 -0400

    Largest Series Product - attempt 2

--- scala/largest-series-product/src/main/scala/Series.scala
+++ scala/largest-series-product/src/main/scala/Series.scala
@@ -1,19 +1,15 @@
-import scala.util.Try
 
 object Series {
   def largestProduct(seriesLength: Int, input: String): Option[Int] = {
-    if (seriesLength > input.length || seriesLength < 0 || !input.forall(Character.isDigit)) return None
+    if (seriesLength > input.length) return None
+    if (seriesLength < 0) return None
+    if (input.exists(!_.isDigit)) return None
     if (seriesLength == 0 || input.isEmpty) return Some(1)
-    val maxStart = input.length - seriesLength
 
-    val candidates: Seq[String] = for {
-      start <- 0 to maxStart
-    } yield {
-      input.substring(start, start + seriesLength)
-    }
+    val candidates = input.sliding(seriesLength, 1).toSeq
 
     val maxProduct = candidates.map { c =>
-      c.toCharArray.map(_.asDigit).product
+      c.map(_.asDigit).product
     }.max
 
     Some(maxProduct)
