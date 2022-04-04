91e076eec8c1ffad34313a84fe4527d9f370b202
Sofia Tania <1446811+stania1@users.noreply.github.com>
2022-04-04 09:19:34 -0400

    Pangram solution

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -5,3 +5,14 @@ Write your code for the 'Pangram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/pangram` directory.
 =end
+
+class Pangram
+
+    ALPHABETS = ('a'..'z').to_a
+
+    def self.pangram?(sentence)
+        return false if sentence.length == 0
+
+        ALPHABETS.map { |a| sentence.downcase.include? a}.all?
+    end
+end
