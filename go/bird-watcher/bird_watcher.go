5bcac7c7de3e96d4931fa8072c588c9e042aff07
Sofia Tania <1446811+stania1@users.noreply.github.com>
2023-01-26 20:51:09 -0500

    Add golang bird watcher

---
+++ go/bird-watcher/bird_watcher.go
@@ -0,0 +1,31 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	sum := 0
+	for _, b := range birdsPerDay {
+		sum += b
+	}
+
+	return sum
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	offset := (week - 1) * 7
+	return TotalBirdCount(birdsPerDay[offset : offset+7])
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := range birdsPerDay {
+		if i%2 == 0 {
+			birdsPerDay[i]++
+		}
+	}
+
+	return birdsPerDay
+}
